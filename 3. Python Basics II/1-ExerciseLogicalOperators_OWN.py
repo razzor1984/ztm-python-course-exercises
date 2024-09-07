d6c86ee2b94e12af64c645e079ce8fc1ac8de5ae
razzor1984 <razzor1984@users.noreply.github.com>
2024-09-07 15:38:59 +0200

    Update 1-ExerciseLogicalOperators_OWN.py

--- 3. Python Basics II/1-ExerciseLogicalOperators_OWN.py
+++ 3. Python Basics II/1-ExerciseLogicalOperators_OWN.py
@@ -10,12 +10,12 @@ if is_magician and is_expert:
   print('you are a master magician')
 
 #if you' re not a magician: "You need magic powers"
-#check if magician but not expert: "at least you're getting there"
 
 if is_magician and is_expert:
   print('you are a master magician')
 
+#check if magician but not expert: "at least you're getting there"
 
-if is_magician not is_expert:
+if is_magician and not is_expert:
   print('at least you're getting there')
 
