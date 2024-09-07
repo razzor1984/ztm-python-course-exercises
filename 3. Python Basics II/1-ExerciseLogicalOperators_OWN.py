a558b42b2594183b800b5e85a3d668a2b5e969e0
razzor1984 <razzor1984@users.noreply.github.com>
2024-09-07 15:36:19 +0200

    Create 1-ExerciseLogicalOperators_OWN.py

---
+++ 3. Python Basics II/1-ExerciseLogicalOperators_OWN.py
@@ -0,0 +1,21 @@
+#Presets
+
+is_magician = True
+is_expert = True
+
+#check if magician and expert : "you are a master magician"
+
+
+if is_magician and is_expert:
+  print('you are a master magician')
+
+#if you' re not a magician: "You need magic powers"
+#check if magician but not expert: "at least you're getting there"
+
+if is_magician and is_expert:
+  print('you are a master magician')
+
+
+if is_magician not is_expert:
+  print('at least you're getting there')
+
